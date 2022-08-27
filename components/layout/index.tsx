931b3f46d1794c726edc9f6a69e236c31318ade2
akhil272 <akhilms272@gmail.com>
2022-08-27 16:39:42 +0530

    added favicons

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import { ReactNode, FC } from "react";
 
@@ -8,6 +9,37 @@ import Header from "./header";
 const Layout: FC<Props> = ({ children }) => {
   return (
     <div>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicons/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/favicons/android-chrome-512x512.png"
+        />
+      </Head>
       <Header />
       <div className="lg:px-36 lg:py-4 p-10 bg-att_black">{children}</div>
       {/* <Footer /> */}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
+import Head from "next/head";
+
 function Homepage() {
   return (
     <div className="bg-att_black text-white lg:py-52 space-y-10 h-screen ">
+      <Head>
+        <title>Attmane | Go Beyond Possibilities </title>
+        <meta
+          name="description"
+          content=" Every business requires a unique solution. And our goal is to develop
+        such unique solutions which will optimize cost, time, and effort. Make
+        your business go beyond possibilities."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <p className="text-lg">Go beyond possibilities</p>
       <div className="flex-col text-5xl capitalize">
         <h4>
